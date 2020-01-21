104ac9edff0c0a9477af2c23333d7d3ecdc0e59f
whwogjs12 <58223134+whwogjs12@users.noreply.github.com>
2020-01-21 16:02:29 +0900

    엑셀 저장을 위한 데이터 생성
    엑셀 불러오기를 통해 리스트를 생성하고 데이터를 저장하기 위해 임시적인 가공

--- src/GUI.java
+++ src/GUI.java
@@ -1,32 +1,89 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.poi.xssf.usermodel.*;
 
-public class GUI extends JFrame
-{
-    JPanel panel = new JPanel();
+
+public class GUI extends JFrame implements ActionListener {
+    JPanel panel = new JPanel(new BorderLayout());
+    JPanel excelPanel = new JPanel();
     JPanel showPanel = new JPanel();
-    public GUI()
-    {
+    WorkingData wd = new WorkingData();
+
+    public GUI() {
         setTitle("근태 포맷 변경 프로그램");
-        setSize(900,600);
+        setSize(1200, 600);
         add(panel);
         init();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    public void init()
-    {
-        GridLayout gl = new GridLayout(2,1);
+    public void init() {
         JButton excelLoading = new JButton("엑셀 불러오기");
         JButton excelSave = new JButton("엑셀 저장하기");
-        panel.add(excelLoading);
-        panel.add(excelSave);
-        panel.add(showPanel);
-        ExcelButton listener = new ExcelButton();
-        excelLoading.addActionListener(listener);
-        excelSave.addActionListener(listener);
+        excelPanel.add(excelLoading);
+        excelPanel.add(excelSave);
+        panel.add(excelPanel, BorderLayout.NORTH);
+        panel.add(showPanel, BorderLayout.CENTER);
+        excelLoading.addActionListener(this);
+        excelSave.addActionListener(this);
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton selectedButton = (JButton) e.getSource();
+        JFileChooser fc = new JFileChooser(new File("c:\\"));
+        fc.setMultiSelectionEnabled(false);
+        fc.setFileFilter(new FilexlsxFilter("xlsx", "excel File"));
+        int result;
+        String filePath = null;
+        if (selectedButton.getText().equals("엑셀 불러오기")) {
+            fc.setDialogTitle("파일 불러오기");
+            result = fc.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                filePath = fc.getSelectedFile().toString();
+                try {
+                    dataTransfer(filePath);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } else {
+            fc.setDialogTitle("파일 저장하기");
+            result = fc.showSaveDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                filePath = fc.getSelectedFile().toString();
+            }
+        }
+        System.out.println(filePath);
+
+    }
+
+    public void dataTransfer(String filePath) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        int rowIndex = sheet.getPhysicalNumberOfRows();
+        wd = new WorkingData();
+        wd.createAttribute(sheet, 0);//첫번째 속성
+        wd.createAttribute(sheet, 1);//두번째 속성
+        wd.createPersonData(sheet, rowIndex);
+
+    }
+
+    public void dataSave(String filePath) throws IOException
+    {
+
+    }
 }
---
+++ src/WorkingData.java
@@ -0,0 +1,109 @@
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+import java.util.ArrayList;
+
+public class WorkingData {
+    private ArrayList<ArrayList<String>> list = null;
+    private ArrayList<String> dateList = null;
+    private ArrayList<String> weekList = null;
+
+    public WorkingData()
+    {
+        list = new ArrayList<ArrayList<String>>();
+        dateList = new ArrayList<String>();
+        weekList = new ArrayList<String>();
+    }
+
+    public ArrayList<ArrayList<String>> getList() {
+        return list;
+    }
+
+    public ArrayList<String> getWeekList() {
+        return weekList;
+    }
+
+    public ArrayList<String> getDateList() {
+        return dateList;
+    }
+
+    public void createAttribute(XSSFSheet sheet, int rowIndex) //맨 위 속성값 생성
+    {
+        XSSFRow row = sheet.getRow(rowIndex);
+        if (row != null) {
+            int column = row.getPhysicalNumberOfCells();
+            if(rowIndex == 0)
+            {
+                dateList = saveColumn(column, row);
+            }
+            else if (rowIndex == 1)
+            {
+                weekList = saveColumn(column, row);
+            }
+        }
+    }
+
+    public void createPersonData(XSSFSheet sheet, int rowIndex)
+    {
+        for(int x= 0;x<rowIndex;x++)
+        {
+            XSSFRow xssfRow = sheet.getRow(x);
+            if(xssfRow !=null)
+            {
+                int columnIndex = xssfRow.getPhysicalNumberOfCells();
+                list.add(saveColumn(columnIndex, xssfRow));
+            }
+        }
+    }
+
+    public ArrayList<String> saveColumn (int columnIndex, XSSFRow row) //각 열에 대한 행 저장
+    {
+        ArrayList<String> returnList = new ArrayList<String>();
+        for(int x = 0;x<columnIndex;x++)
+        {
+            XSSFCell xssfcell = row.getCell(x);
+            String cellData = dataRead(xssfcell);
+            if(cellData !=null)
+            {
+                returnList.add(cellData);
+                //System.out.print(cellData+ "\t");
+            }
+        }
+        //System.out.println("");
+        return returnList;
+    }
+
+    public String dataRead(XSSFCell cell) //셀에 있는 데이터를 String 양식으로 가져오기
+    {
+        String data =  null;
+        try
+        {
+            if(cell.getCellType() == CellType.NUMERIC)
+            {
+                data = Integer.toString((int)cell.getNumericCellValue());
+            }
+            else
+            {
+                data = cell.getStringCellValue();
+            }
+        }
+        catch (NullPointerException e)
+        {
+            data = "";
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public Object [][] listToArray()
+    {
+        Object[][] array = new Object[list.size()][list.get(0).size()];
+        for(int x=1;x<list.size();x++)
+        {
+            array[x] = list.get(x).toArray();
+        }
+        return array;
+    }
+}
